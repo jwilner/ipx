1f8856b09ad1c7eaf48029285bd4c9230d548944
Sergey Polichnoy <pilatuz@gmail.com>
2021-03-08 21:18:48 +0300

    review the `SummarizeRange` function to  check errors on bad input

--- summarize.go
+++ summarize.go
@@ -2,77 +2,134 @@ package ipx
 
 import (
 	"errors"
-	b "math/bits"
+	"fmt"
+	"math/bits"
 	"net"
 )
 
 var v4InV6Prefix = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff}
 
-// SummarizeRange returns a series of networks which combined cover the range between the first and last addresses,
-// inclusive.
-func SummarizeRange(first, last net.IP) []*net.IPNet {
-	four := first.To4() != nil
-	if four != (last.To4() != nil) {
-		return nil // versions must be the same
+// SummarizeRange returns a series of networks which cover the range
+// between the first and last addresses, inclusive.
+func SummarizeRange(first, last net.IP) ([]*Network, error) {
+	// first IPv4 or IPv6
+	var firstV4, firstV6 net.IP
+	switch len(first) {
+	case net.IPv4len:
+		firstV4 = first // first is IPv4
+
+	case net.IPv6len:
+		// note, even if IP address length is 128 it still can be IPv4!
+		// need to do additional check converting it with `To4()`
+		firstV4 = first.To4()
+		if firstV4 == nil {
+			firstV6 = first
 	}
-	if four {
-		return summarizeRange4(to32(first), to32(last))
+
+	default:
+		// invalid first IP address length
+		return nil, fmt.Errorf("%w: first", ErrBadIP)
 	}
-	return summarizeRange6(to128(first), to128(last))
+
+	// last IPv4 or IPv6
+	var lastV4, lastV6 net.IP
+	switch len(last) {
+	case net.IPv4len:
+		lastV4 = last // last is IPv4
+
+	case net.IPv6len:
+		// note, even if IP address length is 128 it still can be IPv4!
+		// need to do additional check converting it with `To4()`
+		lastV4 = last.To4()
+		if lastV4 == nil {
+			lastV6 = last
 }
 
-func summarizeRange4(first, last uint32) (nets []*net.IPNet) {
+	default:
+		// invalid last IP address length
+		return nil, fmt.Errorf("%w: last", ErrBadIP)
+	}
+
+	switch {
+	case firstV4 != nil && lastV4 != nil:
+		return summarizeRange4(to32(firstV4), to32(lastV4)), nil
+	case firstV6 != nil && lastV6 != nil:
+		return summarizeRange6(to128(firstV6), to128(lastV6)), nil
+	}
+
+	return nil, ErrVersionMismatch
+}
+
+// summarizeRange4 returns a series of IPv4 networks which cover the range
+// between the first and last IPv4 addresses, inclusive.
+func summarizeRange4(first, last uint32) (networks []*Network) {
 	for first <= last {
 		// the network will either be as long as all the trailing zeros of the first address OR the number of bits
 		// necessary to cover the distance between first and last address -- whichever is smaller
-		bits := 32
-		if trailingZeros := b.TrailingZeros32(first); trailingZeros < bits {
-			bits = trailingZeros
+		nBits := 32
+		if z := bits.TrailingZeros32(first); z < nBits {
+			nBits = z
 		}
 
 		if first != 0 || last != maxUint32 { // guard overflow; this would just be 32 anyway
-			if diffBits := 31 - b.LeadingZeros32(last-first+1); diffBits < bits {
-				bits = diffBits
+			d := last - first + 1
+			if z := 31 - bits.LeadingZeros32(d); z < nBits {
+				nBits = z
 			}
 		}
 
-		ipN := net.IPNet{IP: make(net.IP, len(net.IPv4zero)), Mask: net.CIDRMask(32-bits, 32)}
+		nwkMask := net.CIDRMask(32-nBits, 32)
+		nwkIP := make(net.IP, net.IPv4len)
+		from32(first, nwkIP)
+		networks = append(networks,
+			&Network{
+				IP:   nwkIP,
+				Mask: nwkMask,
+			})
 
-		ipN.IP = ipN.IP[:4]
-		from32(first, ipN.IP)
-		nets = append(nets, &ipN)
-
-		first += 1 << bits
+		first += 1 << nBits
 		if first == 0 {
 			break
 		}
 	}
+
 	return
 }
 
-func summarizeRange6(first, last uint128) (nets []*net.IPNet) {
-	for first.Cmp(last) != 1 {
-		bits := 128
-		if trailingZeros := trailingZeros128(first); trailingZeros < bits {
-			bits = trailingZeros
+// summarizeRange6 returns a series of IPv6 networks which cover the range
+// between the first and last IPv6 addresses, inclusive.
+func summarizeRange6(first, last uint128) (networks []*Network) {
+	for first.Cmp(last) <= 0 { // first <= last
+		// the network will either be as long as all the trailing zeros of the first address OR the number of bits
+		// necessary to cover the distance between first and last address -- whichever is smaller
+		nBits := 128
+		if z := first.TrailingZeros(); z < nBits {
+			nBits = z
 		}
+
 		// check extremes to make sure no overflow
-		if first.Cmp(uint128{0, 0}) != 0 || last.Cmp(uint128{maxUint64, maxUint64}) != 0 {
-			if diffBits := 127 - leadingZeros128(last.Minus(first).Add(uint128{0, 1})); diffBits < bits {
-				bits = diffBits
+		if !first.Equal(uint128{0, 0}) || !last.Equal(uint128{maxUint64, maxUint64}) {
+			d := last.Minus(first).Add(uint128{0, 1})
+			if z := 127 - d.LeadingZeros(); z < nBits {
+				nBits = z
 			}
 		}
 
-		ipN := net.IPNet{IP: make(net.IP, net.IPv6len), Mask: net.CIDRMask(128-bits, 128)}
+		nwkMask := net.CIDRMask(128-nBits, 128)
+		nwkIP := make(net.IP, net.IPv6len)
+		from128(first, nwkIP)
+		networks = append(networks,
+			&Network{
+				IP:   nwkIP,
+				Mask: nwkMask,
+			})
 
-		from128(first, ipN.IP)
-		nets = append(nets, &ipN)
-
-		first = first.Add(uint128{0, 1}.Lsh(uint(bits)))
-		if first.Cmp(uint128{0, 0}) == 0 {
+		first = first.Add(uint128{0, 1}.Lsh(uint(nBits)))
+		if first.Equal(uint128{0, 0}) {
 			break
 		}
 	}
+
 	return
 }
 
